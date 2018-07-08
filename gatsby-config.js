b5b07ab45e8dbda6beedacd84cfba95db48ce5d1
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2018-07-09 05:43:25 +0700

    Add gh-pages configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Ashari Muhammad Hisbulloh',
   },
+  pathPrefix: '/gatsby-profile',
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
